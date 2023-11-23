3662b401c822b49012253643d3340fd793f391ab
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-11-23 16:12:28 +0900

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DeleteCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,6 +14,10 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        //$schedule->command(ImportCommand::class)->monthly();
+        $schedule->command(DeleteCommand::class)->daily();
+        $schedule->command('sitemap')->monthly();
     }
 
     /**
