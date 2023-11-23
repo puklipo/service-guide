3940c3b5d040b2acd300b1f6b383c2249944d010
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-11-23 15:58:50 +0900

    Update map-pref.blade.php

--- resources/views/livewire/map-pref.blade.php
+++ resources/views/livewire/map-pref.blade.php
@@ -8,13 +8,13 @@ state('pref');
 placeholder('<div class="py-3 text-sm text-gray-400">読み込み中...</div>');
 ?>
 
-<ul class="ml-10 list-disc">
+<ul class="ml-6 list-disc">
     @foreach($pref->areas as $area)
         <li class="my-1">
             <a href="/?pref={{ $pref->id }}&area={{ $area->id }}"
                class="text-indigo-500 underline" wire:navigate>{{ $area->name }}</a>
         </li>
-        <ul class="ml-5">
+        <ul class="ml-3">
             @foreach(config('service') as $service_id => $service)
                 <a href="/?pref={{ $pref->id }}&area={{ $area->id }}&service={{ $service_id }}"
                    class="text-xs hover:text-indigo-500 hover:underline" wire:navigate>{{ $service }}</a>
