94ee9f1e425c21a0ea11da4c67a68003d995f650
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-11-25 14:44:41 +0900

    Update SitemapController.php

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Storage;
 
 class SitemapController extends Controller
@@ -10,8 +11,10 @@ class SitemapController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request): string
+    public function __invoke(Request $request): Response
     {
-        return Storage::get('sitemap.xml');
+        return response(Storage::get('sitemap.xml'), 200, [
+            'Content-Type' => 'text/xml',
+        ]);
     }
 }
