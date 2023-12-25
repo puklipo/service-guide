86b750187c99d043a2e43b357cc94b046d8bb08e
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-12-26 08:19:38 +0900

    Update map-service.blade.php

--- resources/views/livewire/map-service.blade.php
+++ resources/views/livewire/map-service.blade.php
@@ -5,9 +5,13 @@ use function Livewire\Volt\state;
 state(['pref', 'area']);
 ?>
 
-<div class="ml-3">
-    @foreach(config('service') as $service_id => $service)
-        <a href="/?pref={{ $pref }}&area={{ $area }}&service={{ $service_id }}"
-           class="text-xs hover:text-indigo-500 hover:underline" wire:key="{{ $service_id }}" wire:navigate>{{ $service }}</a>
-    @endforeach
+<div class="ml-3 mb-3">
+    <details>
+        <summary>サービスを表示</summary>
+        @foreach(config('service') as $service_id => $service)
+            <a href="/?pref={{ $pref }}&amp;area={{ $area }}&amp;service={{ $service_id }}"
+               class="text-sm hover:text-indigo-500 hover:underline" wire:key="{{ $service_id }}"
+               wire:navigate>{{ $service }}</a>
+        @endforeach
+    </details>
 </div>
