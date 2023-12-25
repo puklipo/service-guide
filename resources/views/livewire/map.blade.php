e2d09de344481472e4580d8c955d06cfe1e79dad
pop-culture-studio <pcs.engineer.team@gmail.com>
2023-12-26 08:21:22 +0900

    Update map.blade.php

--- resources/views/livewire/map.blade.php
+++ resources/views/livewire/map.blade.php
@@ -32,7 +32,7 @@ $prefs = computed(function () {
             <ul class="ml-6 list-disc">
                 @foreach($pref->areas as $area)
                     <li class="my-1" wire:key="{{ $area->id }}">
-                            <a href="/?pref={{ $pref->id }}&area={{ $area->id }}"
+                            <a href="/?pref={{ $pref->id }}&amp;area={{ $area->id }}"
                                class="text-indigo-500 underline" wire:navigate>{{ $area->name }}</a>
                             <livewire:map-service :pref="$pref->id" :area="$area->id" lazy></livewire:map-service>
                     </li>
