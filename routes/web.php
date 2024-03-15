f6f008356f112a2b80e7c1e083a6f623501f410e
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-03-15 12:52:17 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,17 +5,6 @@ use App\Livewire\Home;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', Home::class)->name('home');
 
 Volt::route('s/{facility}', 'facility')
