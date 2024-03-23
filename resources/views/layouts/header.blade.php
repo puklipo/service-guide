1583280ffaeceafddd7321e7d7e1e7a3c6dc1b04
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-03-23 09:48:42 +0900

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,6 +11,6 @@
     </h1>
 
     <div class="text-sm my-2 py-2 px-2 ring-1 ring-indigo-500">
-        <a href="https://www.wam.go.jp/sfkohyoout/" class="text-indigo-500 hover:underline" target="_blank">WAM NET</a>のオープンデータを基にした障害福祉サービスの検索サイトです。自治体ごとの事業所の一覧を表示するまでを目的にしているので各事業所の詳細は公式サイトやWAMを検索してください。共同生活援助は専用の<a href="https://grouphome.guide/" class="text-indigo-500 hover:underline" target="_blank">障害者グループホームガイド</a>もあります。
+        <a href="https://www.wam.go.jp/sfkohyoout/" class="text-indigo-500 hover:underline" target="_blank">WAM NET</a>のオープンデータを基にした障害福祉サービスの検索サイトです。自治体ごとの事業所の一覧を表示するまでを目的にしているので各事業所の詳細は公式サイトやWAMを検索してください。事業所の情報を追加することも可能です。共同生活援助は専用の<a href="https://grouphome.guide/" class="text-indigo-500 hover:underline" target="_blank">障害者グループホームガイド</a>もあります。
     </div>
 </header>
