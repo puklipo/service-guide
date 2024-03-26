5a5299c730a51daa24132ff546c071f2170e8956
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-03-26 12:45:31 +0900

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
+use Puklipo\Vapor\Middleware\GzipResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,5 +28,14 @@ class AppServiceProvider extends ServiceProvider
         Model::shouldBeStrict(! $this->app->isProduction());
 
         Gate::define('admin', fn (User $user) => 1);
+
+        GzipResponse::encodeWhen(function (Request $request, mixed $response): bool {
+            return in_array('gzip', $request->getEncodings())
+                && $request->method() === 'GET'
+                && ! $request->hasHeader('X-Livewire')
+                && function_exists('gzencode')
+                && ! $response->headers->contains('Content-Encoding', 'gzip')
+                && ! $response instanceof BinaryFileResponse;
+        });
     }
 }
