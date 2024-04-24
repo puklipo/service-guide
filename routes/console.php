4975bb76f277bea06b97f1f64aa2ab1d78851ac8
pop-culture-studio <pcs.engineer.team@gmail.com>
2024-04-24 18:36:07 +0900

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -17,5 +17,3 @@ Artisan::command('sitemap', function () {
 
 Schedule::command(ImportCommand::class)->monthly();
 Schedule::command(DeleteCommand::class)->monthlyOn(2);
-
-Schedule::command('queue:prune-batches')->daily();
