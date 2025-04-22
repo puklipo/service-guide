e5815537f69fab6cd298972bcb80a296ffd36944
puklipo <pcs.engineer.team@gmail.com>
2025-04-22 12:34:16 +0900

    Update textarea.blade.php

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -1,3 +1,3 @@
 <textarea
-    {{ $attributes->merge(['class' => 'my-1 block w-full bg-white text-black border-gray-300 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-300 focus:border-indigo-300 focus:ring-3 focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-none']) }} rows="4" cols="40"
+    {{ $attributes->merge(['class' => 'textarea']) }} rows="4" cols="40"
 ></textarea>
