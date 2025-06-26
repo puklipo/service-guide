5ff9b7286097698fc51c8bad0d0bc4e848717f72
puklipo <pcs.engineer.team@gmail.com>
2025-06-26 14:37:18 +0900

    Update show.blade.php

--- resources/views/livewire/articles/show.blade.php
+++ resources/views/livewire/articles/show.blade.php
@@ -8,33 +8,38 @@ use function Livewire\Volt\{state};
 use function Livewire\Volt\layout;
 use function Livewire\Volt\mount;
 use function Livewire\Volt\title;
+use function Livewire\Volt\{computed};
 
 layout('layouts.app');
 
-state(['date', 'title', 'description', 'body']);
+state(['date', 'slug', 'file', 'title', 'description']);
 
 mount(function (string $date, string $slug) {
-    $file = resource_path('articles/'.$date.'/'.$slug.'.md');
-    if (! file_exists($file)) {
+    $this->file = resource_path('articles/'.$date.'/'.$slug.'.md');
+    if (! file_exists($this->file)) {
         return to_route('home');
     }
 
-    $document = YamlFrontMatter::parseFile($file);
-    $this->title = $document->matter('title') ?? 'Article';
-    $this->description = $document->matter('description') ?? 'No description provided.';
-    $this->body = $document->body();
+    $document = YamlFrontMatter::parseFile($this->file);
+    $this->title = $document->matter('title');
+    $this->description = $document->matter('description');
+});
+
+$document = computed(function () {
+    return YamlFrontMatter::parseFile($this->file);
 });
 
 title(fn () => $this->title);
 
 ?>
+<x-slot:description>{{ $description }}</x-slot>
 
 <div class="mx-1 sm:mx-10">
     @include('layouts.header')
 
     <div class="prose max-w-4xl mx-6 lg:mx-auto my-12">
         <article class="prose">
-            {{ Markdown::parse($body) }}
+            {{ Markdown::parse($this->document->body()) }}
         </article>
 
         <div class="my-6">
